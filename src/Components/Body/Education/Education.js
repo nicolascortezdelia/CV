465b791891b1899ccd1179129e31578db3ab0779
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-01-17 14:20:33 -0300

    agrego contenido a componente Education

--- src/Components/Body/Education/Education.js
+++ src/Components/Body/Education/Education.js
@@ -21,27 +21,27 @@ const Education = () => {
         {/* Content education */}
         <div className="row content-sections">
           <div className="col s12 m4 xl3 center-align bg-years">
-            <h6>2019 - 2020</h6>
+            <h6>2021 - Present</h6>
           </div>
           <div className="col s12 m8 xl9 content-border-left">
             <div className="circles"></div>
             <h6>Rolling Code School</h6>
-            <p>Completed full stack web developer course.</p>
+            <p>Full stack web developer course in progress.</p>
           </div>
         </div>
       </Fade>
       <Fade bottom>
         <div className="row content-sections">
           <div className="col s12 m4 xl3 center-align bg-years">
-            <h6>2006 - 2011</h6>
+            <h6>2021</h6>
           </div>
           <div className="col s12 m8 xl9 content-border-left">
             <div className="circles"></div>
-            <h6>High School</h6>
+            <h6>Postgraduate Course</h6>
             <p>
-              Training institution: Liceo Remedio de Escalada de San Martin.
-              <br />
-              Degree: Polymodal Education (Orientation in Natural Sciences).
+             Personal Data Protection at Austral University 
+             
+              
             </p>
           </div>
         </div>
@@ -49,12 +49,44 @@ const Education = () => {
       <Fade bottom>
         <div className="row content-sections">
           <div className="col s12 m4 xl3 center-align bg-years">
-            <h6>1998 - 2005</h6>
+            <h6>2020-2021</h6>
           </div>
           <div className="col s12 m8 xl9 content-border-left">
             <div className="circles"></div>
-            <h6>Primary School</h6>
-            <p>School: Dr. Raúl Colombres.</p>
+            <h6>Postgraduate Course</h6>
+            <p>
+            Law, Innovation and Technology at University of Tucuman
+             
+              
+            </p>
+          </div>
+        </div>
+      </Fade>
+      <Fade bottom>
+        <div className="row content-sections">
+          <div className="col s12 m4 xl3 center-align bg-years">
+            <h6>2011-2017</h6>
+          </div>
+          <div className="col s12 m8 xl9 content-border-left">
+            <div className="circles"></div>
+            <h6>Law Degree</h6>
+            <p>
+              Law School University of Tucuman
+              <br />
+              
+            </p>
+          </div>
+        </div>
+      </Fade>
+      <Fade bottom>
+        <div className="row content-sections">
+          <div className="col s12 m4 xl3 center-align bg-years">
+            <h6>2005 - 2010</h6>
+          </div>
+          <div className="col s12 m8 xl9 content-border-left">
+            <div className="circles"></div>
+            <h6>High School</h6>
+            <p> Colegio Tulio García Fernández</p>
           </div>
         </div>
       </Fade>
